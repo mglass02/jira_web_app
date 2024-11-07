bfefc37e7e7b85c6dbf8067a1a8c653d581e706f
micha <michael.andrew.glass@gmail.com>
2024-11-07 00:39:55 +0000

    added ability to link files from public

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
